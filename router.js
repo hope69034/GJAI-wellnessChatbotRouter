dc4c48e8529969042140beb9d64e355ede8ec8de
sehyoungchoi <hope69034@gmail.com>
2022-11-15 11:38:11 +0900

    3

--- router.js
+++ router.js
@@ -1,5 +1,9 @@
+
+const { spawn } = require("child_process");
+
 const express = require('express');
 const router = express.Router();
+
 
 router.get('/', function (req, res) {    
   res.redirect("http://127.0.0.1:5500/greeni.html")
@@ -7,7 +11,8 @@ router.get('/', function (req, res) {
 
 router.post('/Greeni', function (req, res) {    
   let userInput = req.body.userInput
-  const spawn = require("child_process").spawn;
+  let data = "";
+  // const spawn = require("child_process").spawn;
   const outp = spawn("python", ["talkmodel.py", userInput]);
   console.log('greeni라우터진입: 나 : '+ userInput + ', 타입: ' + typeof(userInput))
   outp.stdout.on("data", (result) => {
