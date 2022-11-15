e3a509eb69b3e3965412c5b4e773392799b97076
sehyoungchoi <hope69034@gmail.com>
2022-11-15 12:10:10 +0900

    12

--- router.js
+++ router.js
@@ -10,14 +10,16 @@ router.get('/', function (req, res) {
 });
 
 router.post('/Greeni', function (req, res) {    
+  console.log('라우터1')
   let userInput = req.body.userInput
   let data = "";
+  console.log('라우터2')
   // const spawn = require("child_process").spawn;
-  const outp = spawn("python", ["talkmodel.py", userInput]);
-  console.log('greeni라우터진입: 나 : '+ userInput + ', 타입: ' + typeof(userInput))
-  outp.stdout.on("data", (result) => {
-    console.log('그리니 : '+result.toString());
-    res.json(result.toString())
+  const outp = spawn("python3", ["talkmodel.py", userInput]);
+    console.log('greeni라우터진입: 나 : '+ userInput + ', 타입: ' + typeof(userInput))
+    outp.stdout.on("data", (result) => {
+      console.log('그리니 : '+result.toString());
+      res.json(result.toString())
   });
 });
 
