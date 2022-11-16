aaf5dce392569323a1f0b7a2ed3e736f9153d65d
sehyoungchoi <hope69034@gmail.com>
2022-11-16 14:44:21 +0900

    1231

--- router.js
+++ router.js
@@ -10,29 +10,29 @@ router.get('/', function (req, res) {
 });
 
 router.post('/Greeni', function (req, res) {    
-  console.log('라우터1')
+  console.log('1. 라우터진입')
   let userInput = req.body.userInput
   let data = "";
-  console.log('라우터2')
+  console.log('2. 바디파서')
   // const spawn = require("child_process").spawn;
   // aws ec2서버에는 python3가 깔려있어서 3로
   //const outp = spawn("python3", ["talkmodel.py", userInput]);
   const outp = spawn("python3", ["talkmodel.py"]);
-  console.log('talkmodel 변수선언성공 stdout 진입 아직 안함 = 나 : '+ userInput + ', 타입: ' + typeof(userInput))
+  console.log('3. talkmodel 변수선언성공 stdout 진입 아직 안함 = 나 : '+ userInput + ', 타입: ' + typeof(userInput))
   
-  try {
+ 
 
   outp.stdout.on("data", (result) => {
     //console.error('에러남')
-    console.log('stdout 진입')
+    console.log('4. stdout 진입 성공 ')
     //console.log('그리니 : '+result.toString());
     res.json(result.toString())
   });
 
-  } catch (e) {
-    console.error(e);
+  
+    console.error('에러남');
     res.json('음!!')
-  }
+ 
 
   
 });
